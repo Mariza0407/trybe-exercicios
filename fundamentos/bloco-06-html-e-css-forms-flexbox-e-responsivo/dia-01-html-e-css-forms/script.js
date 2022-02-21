f1f21370be051060eb26ac60485df4d45aa6f0ae
Mariza Paini <marizapaini83@gmail.com>
2022-02-21 10:10:49 -0300

    Adiciona exercicio 6.1 Forms

---
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/script.js
@@ -0,0 +1,26 @@
+function SubmitButton(event) {
+    event.preventDefault();
+    window.onload = function () {
+        const submitBtn = document.querySelector('#submitBt');
+        submitBtn.addEventListener('click', SubmitButton);
+    };
+}
+
+function clearButton(event) {
+    event.preventDefault();
+    const formElements = document.querySelectorAll('input');
+    const textArea = document.querySelector('textarea');
+    for (let index = 0; index < formElements.length; index += 1) {
+        const userInput = formElements[index];
+        userInput.value = '';
+        userInput.checked = false;
+    }
+    textArea.value = '';
+}
+window.onload = function (event) {
+    event.preventDefault();
+    const clearBtn = document.querySelector('#clear-btn');
+    clearBtn.addEventListener('click', clearButton);
+    const submitBtn = document.querySelector('#submitBt');
+    submitBtn.addEventListener('click', SubmitButton);
+};

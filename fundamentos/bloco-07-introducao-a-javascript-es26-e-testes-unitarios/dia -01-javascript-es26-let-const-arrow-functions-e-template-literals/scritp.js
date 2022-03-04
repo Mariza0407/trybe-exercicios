fe4734d979bc1e14116cee1fe430b20ece6dd6f9
Mariza Paini <marizapaini83@gmail.com>
2022-03-04 10:13:09 -0300

    Adiciona corpo dos exercicios do dia 01

--- fundamentos/bloco-07-introducao-a-javascript-es26-e-testes-unitarios/dia -01-javascript-es26-let-const-arrow-functions-e-template-literals/scritp.js
+++ fundamentos/bloco-07-introducao-a-javascript-es26-e-testes-unitarios/dia -01-javascript-es26-let-const-arrow-functions-e-template-literals/scritp.js
@@ -49,3 +49,44 @@ const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 // console.log(`Os números do array ${sortDoArray} em ordem crescente são!`);
 //OOUU
 console.log(`Os números do array ${oddsAndEvens.sort((a, b) => a -b)} em ordem crescente são!`);
+
+//PARTE II
+//EXERCICIO 01
+// Crie uma função que receba um número e retorne seu fatorial.
+// Na matemática, o fatorial de um número não negativo N , com a notação N! , é o
+//  produto de todos os inteiros menores ou iguais a N . Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+// Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . Spoiler: É possível
+//  resolver com uma linha usando ternary operator .
+
+
+//EXERCICIO 02
+// Crie uma função que receba uma frase e retorne qual a maior palavra.
+longestWord('Antônio foi no banheiro e não sabemos o que aconteceu') // retorna 'aconteceu'
+
+
+
+//EXERCICIO 03
+// Crie uma página que contenha:
+// Um botão ao qual será associado um event listener ;
+// Uma variável clickCount no arquivo JavaScript que acumule o número de clicks no botão;
+// Um campo no HTML que vá atualizando a quantidade de clicks no botão conforme a variável
+//  clickCount é atualizada.
+
+
+//EXERCICIO 04
+// Crie um código JavaScript com a seguinte especificação:
+// Não se esqueça de usar template literals
+// Função 1 : Escreva uma função que vai receber uma string como parâmetro. Sua função deverá procurar pela 
+// letra x em uma string qualquer que você determinar e substituir pela string que você passou como parâmetro. Sua 
+// função deve retornar essa nova string .
+// Exemplo:
+// String determinada: "Tryber x aqui!"
+// Parâmetro: "Bebeto"
+// Retorno: "Tryber Bebeto aqui!"
+// Um array com escopo global, que é o escopo do arquivo JS , nesse caso, contendo cinco strings com suas principais skills .
+// Função 2 : Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve 
+// concatenar as skills do array global à string que foi passada para a Função 2 via parâmetro. Você 
+// deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string .
+// Exemplo: "Tryber x aqui! Minhas cinco principais habilidades são:
+// JavaScript;
+// HTML; ... #goTrybe".
